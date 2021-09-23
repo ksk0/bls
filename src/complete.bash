240134eac303d0435a718b478e46a387c8c0a2bf
koske <ksk0@users.noreplay.github.com>
2021-09-23 13:31:36 +0200

    Root commit of bls script

---
+++ src/complete.bash
@@ -0,0 +1,20 @@
+__bls_complete(){
+	[[ $COMP_CWORD -gt 2 ]] && return
+
+	if [[ $COMP_CWORD -eq 1 ]]; then
+		COMPREPLY=($(compgen -W "backup chroot restore list mount info" $2))
+		return
+	fi
+
+
+	[[ $3 != backup ]] && return
+	[[ ! -f /etc/bls.conf ]] && return
+
+	COMPREPLY=($(
+		compgen -W "$(
+			awk '/^[ 	]*\[[ 	]*[^ 	]+[ 	]*\][ 	]*(#.*)?$/{print $2}' /etc/bls.conf
+		)" $2
+	))
+}
+
+complete -F __bls_complete bls
---
+++ src/complete.zsh
@@ -0,0 +1,22 @@
+#compdef bls
+
+local -a reply
+local -a args=(/$'[^\0]#\0'/)
+
+
+local -a bls_commands
+
+_regex_words bls-commands "bls commands" \
+	'info:show exetended info on command usage' \
+	'chroot:chroot to restored system' \
+	'backup:backup current system' \
+	'restore:restore system from backup' \
+	'list:list backups from selected server' \
+	'mount:mount server share localy'
+
+	args+=($reply[@])
+
+
+_regex_arguments _bls "${args[@]}"
+
+_bls "$@"
